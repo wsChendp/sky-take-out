5e8db4c25245bbe76640859ed9233cf2b533fdde
wsChendp <117282470+wsChendp@users.noreply.github.com>
2024-02-04 22:36:15 +0800

    新增菜品代码开发
    有图片托管，动态sql，lamba表达式

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,11 +1,13 @@
 package com.sky.mapper;
 
+import com.sky.annocation.AutoFill;
+import com.sky.entity.Dish;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface DishMapper {
-
     /**
      * 根据分类id查询菜品数量
      * @param categoryId
@@ -14,4 +16,6 @@ public interface DishMapper {
     @Select("select count(id) from dish where category_id = #{categoryId}")
     Integer countByCategoryId(Long categoryId);
 
+    @AutoFill(value = OperationType.INSERT)
+    void insert(Dish dish);
 }
